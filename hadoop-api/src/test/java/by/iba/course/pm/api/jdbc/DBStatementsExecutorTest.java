3384c30c156a25a0fa5155b15d1a45e0774a2307
AndrewLoginov <andrew.rsq@gmail.com>
2013-10-16 02:52:16 +0300

    added test and several fixes

--- hadoop-api/src/test/java/by/iba/course/pm/api/jdbc/DBStatementsExecutorTest.java
+++ hadoop-api/src/test/java/by/iba/course/pm/api/jdbc/DBStatementsExecutorTest.java
@@ -4,8 +4,6 @@ package by.iba.course.pm.api.jdbc;
 import by.iba.course.pm.api.jdbc.stmt.impl.InsertLinkStatement;
 import junit.framework.TestCase;
 
-import java.sql.SQLException;
-
 /**
  * @Author: Andrew
  * @Date: 10/16/13
@@ -20,13 +18,6 @@ public class DBStatementsExecutorTest extends TestCase {
 			dbStatementsExecutor.executeStatement(new InsertLinkStatement("ibm.com"));
 			dbStatementsExecutor.executeStatement(new InsertLinkStatement("ibm.com"));
 		} catch (Exception e) {
-			e.printStackTrace();
-			fail(e.getMessage());
-		}
-		try {
-			dbStatementsExecutor.executeStatement(new InsertLinkStatement("ibm.com"));
-		} catch (Exception e) {
-			e.printStackTrace();
 			fail(e.getMessage());
 		}
 	}
