1aa75bf5f42fe5fb90f0cf6b5175b678a9178ee7
AndreiLohinau <AndreiLohinau@github.com>
2013-10-16 15:28:39 +0300

    added sample run of external hadoop jar + recofigured maven build for manager

--- hadoop-manager/src/main/java/by/iba/course/pm/aggr/CentralisedExecutor.java
+++ hadoop-manager/src/main/java/by/iba/course/pm/aggr/CentralisedExecutor.java
@@ -1,6 +1,14 @@
 package by.iba.course.pm.aggr;
 
 
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.util.RunJar;
+
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @Author: Andrew
  * @Date: 10/14/13
@@ -8,4 +16,18 @@ package by.iba.course.pm.aggr;
 
 
 public class CentralisedExecutor {
+	public static void main(String[] args) throws Throwable {
+
+		List<String> arguments = new LinkedList<String>();
+
+		//arguments.add("hadoop-cyclic-job.jar");
+		arguments.add("hadoop-examples-1.2.1.jar");
+
+		arguments.add("grep");
+		arguments.add("CHANGES.txt");
+		arguments.add("output2");
+		arguments.add("'dfs[a-z.]+'");
+
+		RunJar.main(arguments.toArray(new String[arguments.size()]));
+	}
 }
